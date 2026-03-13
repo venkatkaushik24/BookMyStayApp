33e95609f54a41a0124bce189a1e6c5d301e1e44
venkatkaushik24 <venkatkaushik24@gmail.com>
2026-03-13 14:28:20 +0530

    UC1: Application entry and welcome message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,23 @@
+/**
+ * BookMyStay Application
+ * This class represents the entry point of the Hotel Booking System.
+ * It prints the welcome message along with application name and version.
+ *
+ * @author Lakshmi
+ * @version 1.0
+ */
+
 public class BookMyStayApp {
+
+    /**
+     * Main method - Entry point of the Java application
+     */
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println(" Welcome to BookMyStay ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("==================================");
+
+    }
 }
